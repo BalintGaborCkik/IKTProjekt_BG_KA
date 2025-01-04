62347a0603f5d6d3ebddcd42bedb940601c21425
Gabor <balintgabor04@gmail.com>
2025-01-04 12:00:55 +0100

    a) feladat érdeni kommit (BG)

--- IKTProjekt_7_BG_Ka.py
+++ IKTProjekt_7_BG_Ka.py
@@ -1,3 +1,13 @@
 #Bálint Gábor 10.B és Kaibás Alex 10.B - Python első beadandó
+print("a) feladat --------------------")
 n=8
 t=[6400, -2000, -4300, 8200, 1000, -3400, 600, -900]
+maxny=0
+maxindex=0
+nyossz=0
+for i in range(n):
+    nyossz+=t[i]
+    if maxny < nyossz:
+        maxny = nyossz
+        maxindex = i+1
+print(maxindex,". héten volt a legnagyobb a nyereségünk.")
